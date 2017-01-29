d327a85cb26922fec9f41616de659e2d9a38fafa
Dave Falkner <ardave@gmail.com>
2017-01-28 19:37:02 -0800

    more compiler warning fixes

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,11 +1,11 @@
 extern crate i2cdev;
-extern crate BME280;
+extern crate bme280;
 
 use std::error::Error;
 
 #[test]
 fn it_can_initialize() {
-    let result = BME280::create(0x77, 2);
+    let result = bme280::create(0x77, 2);
     match result {
         Ok(device) => assert!(true),
         Err(err) => {
@@ -20,7 +20,7 @@ fn it_can_initialize() {
 
 #[test]
 fn temperature_reading_should_be_reasonable() {
-    let mut device = BME280::create(0x77, 2).unwrap();
+    let mut device = bme280::create(0x77, 2).unwrap();
     let t = device.read_temperature().unwrap();
     println!("The temperature is: {:.2}", t);
     assert!(t > -50.0); // I'm starting out thinking fahrenheit, but we'll get there.
@@ -29,7 +29,7 @@ fn temperature_reading_should_be_reasonable() {
 
 #[test]
 fn pressure_reading_should_be_reasonable() {
-    let mut device = BME280::create(0x77, 2).unwrap();
+    let mut device = bme280::create(0x77, 2).unwrap();
     let t = device.read_pressure().unwrap();
     println!("The pressure is: {:.2} in hg.", t);
     assert!(t > -25.0); 
