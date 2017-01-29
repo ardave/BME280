5a6f2e1cf46158c73877460b68a524b7bcbf0195
Dave Falkner <ardave@gmail.com>
2017-01-28 18:30:48 -0800

    improved string formatting in test output messages

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -22,7 +22,7 @@ fn it_can_initialize() {
 fn temperature_reading_should_be_reasonable() {
     let mut device = BME280::create(0x77, 2).unwrap();
     let t = device.read_temperature().unwrap();
-    println!("The temperature is: {}", t);
+    println!("The temperature is: {:.2}", t);
     assert!(t > -50.0); // I'm starting out thinking fahrenheit, but we'll get there.
     assert!(t < 130.0);
 }
@@ -31,7 +31,7 @@ fn temperature_reading_should_be_reasonable() {
 fn pressure_reading_should_be_reasonable() {
         let mut device = BME280::create(0x77, 2).unwrap();
     let t = device.read_pressure().unwrap();
-    println!("The pressure is: {} in hg.", t);
+    println!("The pressure is: {:.2} in hg.", t);
     assert!(t > -25.0); 
     assert!(t < 35.0);
 }
