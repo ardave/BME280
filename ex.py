7c8c8772eaa298c13a3cdb052b860181811abb98
David Falkner <ardave@gmail.com>
2017-02-28 21:35:24 -0800

    debugging

--- ex.py
+++ ex.py
@@ -2,17 +2,17 @@ from Adafruit_BME280 import *
 
 sensor = BME280(mode=BME280_OSAMPLE_8, busnum=2)
 
-pascals = sensor.read_pressure()
+# pascals = sensor.read_pressure()
 degrees = sensor.read_temperature()
 
 hectopascals = pascals / 100
 inhg = hectopascals / 33.8638866667
-humidity = sensor.read_humidity()
+# humidity = sensor.read_humidity()
 
-print 'Timestamp = {0:0.3f}'.format(sensor.t_fine)
-print 'Temp      = {0:0.3f} deg C'.format(degrees)
-print 'Pressure  = {0:0.2f} hPa'.format(inhg)
-print 'Humidity  = {0:0.2f} %'.format(humidity)
+# print 'Timestamp = {0:0.3f}'.format(sensor.t_fine)
+# print 'Temp      = {0:0.3f} deg C'.format(degrees)
+# print 'Pressure  = {0:0.2f} hPa'.format(inhg)
+# print 'Humidity  = {0:0.2f} %'.format(humidity)
 
 pascals = sensor.read_pressure()
 hectopascals = pascals / 100
